766900be8f014e200af8ef3adac21d780fe4624e
Thomas <thomas.abbondi@crystalware.cloud>
2019-03-20 20:09:47 +0100

    Client script Refactoring

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -3,109 +3,219 @@
  * _beng$uid: user ID / server socket ID
  * _beng$pid: page ID
  */
-(function(debug) {
-  var _beng$wid;
-  var _beng$uid;
-  var _beng$pid = btoa(window.location.pathname);
+var _bengala = (function() {
+  'use strict';
 
-  var domain = 'bengala.crystalware.test';
-  var api = {
-    'auth': '/auth'
-  };
-
-  var ports = {
-    'ws': 9080,
-    'wss': 9443,
-    'http': 80,
-    'https': 443,
-    'dev': 3000,
-  };
-
-  var clockRate = 1000;
-
-  function waitId() {
-    if (typeof _beng$wid !== "undefined") {
-      if (debug) var listener = document.getElementById('listener');
-
-      if (!Date.now) {
-        Date.now = function() {
-          return new Date().getTime();
-        }
-      }
-
+  var _protected = {
+    create: function(uri) {
       if (window.WebSocket) {
-        var ws = new WebSocket('ws://bengala.crystalware.test:9080');
+        try {
+          return new WebSocket(uri);
+        } catch (error) {
+          console.error('Failed to open WebSocket:', error);
+          return false;
+        }
+      } else {
+        console.warn('WebSocket not supported');
+        return false;
+      }
+    },
 
-        ws.onmessage = function(message) {
-          let data = message.data;
+    errorHandler: function(error) {
+      console.error('WebSocket error: ', error);
+    },
 
-          try {
-            data = JSON.parse(message.data);
-          } catch (e) {
-            if (debug) console.warn('Error during parse message to JSON');
-          } finally {
-            if (debug) console.log(data);
-            if (debug) debuggerMsg(message);
+    closeHandler: function() {
+      console.info('WebSocket is closed');
+    },
 
-            if (_beng$uid === undefined && data.uid !== undefined && data.uid !== false) {
-              _beng$uid = data.uid;
-              if (debug) console.log(_beng$uid);
+    connect: function(ws, clock, bengala) {
+      ws.onmessage = function(message) {
+        var data;
+
+        try {
+          data = JSON.parse(message.data);
+        } catch (e) {
+          return;
+        }
+
+        if (data.uid != null && bengala.$uid == null) {
+          console.log(data.uid);
+          bengala.setUserId(data.uid);
+        }
+      };
+
+      ws.onerror = this.errorHandler;
+      ws.onclose = this.closeHandler;
+
+      ws.onopen = function() {
+        console.info('WebSocket is connected');
+
+        var init = {
+          ts: Date.now(),
+          wid: bengala.$wid,
+          pid: bengala.$pid()
+        };
+
+        if (bengala.checkUserId()) {
+          init.uid = bengala.$uid;
+        }
+
+        ws.send(JSON.stringify(init), function(error) {
+          if (error == undefined) {
+            return;
+          } else {
+            console.error('Async error: ' + error);
+          }
+        });
+      };
+
+      var int = setInterval(function() {
+        var heart_beat = {
+          ts: Date.now(),
+          wid: bengala.$wid,
+          uid: bengala.$uid,
+          pid: bengala.$pid()
+        };
+
+        if (ws.readyState === WebSocket.OPEN) {
+          ws.send(JSON.stringify(heart_beat), function(error) {
+            if (error == undefined) {
+              return;
+            } else {
+              console.error('Async error: ' + error);
+            }
+          });
+        } else {
+          clearInterval(int);
+        }
+      }, clock);
+    }
+  };
+
+  var bengala = {
+    _beng: {
+      $wid: null,
+      $uid: null,
+      $pid: function() {
+        return btoa(window.location.pathname);
+      },
+      setUserId: function(uid) {
+        this.$uid = uid;
+        var d = new Date();
+        d.setTime(d.getTime() + (60 * 60 * 1000));
+        var expires = "expires=" + d.toUTCString();
+        document.cookie = this.prefix + this.$uid + '=' + encodeURIComponent(this.$pid()) + '; expires=' + expires + '; path=/';
+      },
+      checkUserId: function() {
+        var cookies = document.cookie.split(';');
+
+        if (cookies.length > 0) {
+          for (var i = cookies.length - 1; i >= 0; i--) {
+            var cookie = cookies[i].trim().split('=');
+
+            if (cookie.length > 0) {
+              var user = cookie[0].trim().split('_');
+
+              if (user[0] == this.prefix) {
+                this.setUserId(user[1]);
+                return true;
+              }
             }
           }
-        };
+        }
 
-        ws.onerror = function(error) {
-          console.error('WebSocket error: ', error);
-        };
-
-        ws.onclose = function() {
-          if (debug) console.info('WebSocket is closed...');
-        };
-
-        ws.onopen = function() {
-          if (debug) console.info('WebSocket is connected...');
-
-          var init = {ts: Date.now(), wid: _beng$wid, pid: _beng$pid};
-          ws.send(JSON.stringify(init));
-        };
-
-        var int = setInterval(function() {
-          if (debug) console.log(ws.readyState);
-          var heart_beat = {
-            ts: Date.now(),
-            wid: _beng$wid,
-            uid: _beng$uid,
-            pid: _beng$pid
-          };
-
-          if (ws.readyState === WebSocket.OPEN) {
-            ws.send(JSON.stringify(heart_beat), function(error) {
-              if (error == undefined) {
-                return;
-              } else {
-                console.error('Async error: ' + error);
-              }
-            });
-          } else {
-            clearInterval(int);
-          }
-        }, clockRate);
-      } else {
-        var ws;
-        console.warn('WebSocket not supported');
+        return false;
       }
-    } else {
-      setTimeout(waitId, 250);
-    }
-  }
+    },
 
-  function debuggerMsg(msg) {
-    var node = document.createElement("LI");
-    var textnode = document.createTextNode(msg.data);
-    node.appendChild(textnode);
+    prefix: 'id-user-beng_',
 
-    listener.appendChild(node);
-  }
+    domain: 'bengala.crystalware.test',
+    ports: {
+      'ws': 9080,
+      'wss': 9443
+    },
 
-  waitId();
-})(true);
+    getPath: function(secure = false) {
+      var protocols = (function(object) {
+        if (!Object.keys) {
+          Object.keys = (function() {
+            'use strict';
+            var hasOwnProperty = Object.prototype.hasOwnProperty,
+              hasDontEnumBug = !({toString: null}).propertyIsEnumerable('toString'),
+              dontEnums = [
+                'toString',
+                'toLocaleString',
+                'valueOf',
+                'hasOwnProperty',
+                'isPrototypeOf',
+                'propertyIsEnumerable',
+                'constructor'
+              ],
+              dontEnumsLength = dontEnums.length;
+
+            return function(obj) {
+              if (typeof obj !== 'object' && (typeof obj !== 'function' || obj === null)) {
+                throw new TypeError('Object.keys called on non-object');
+              }
+
+              var result = [], prop, i;
+
+              for (prop in obj) {
+                if (hasOwnProperty.call(obj, prop)) {
+                  result.push(prop);
+                }
+              }
+
+              if (hasDontEnumBug) {
+                for (i = 0; i < dontEnumsLength; i++) {
+                  if (hasOwnProperty.call(obj, dontEnums[i])) {
+                    result.push(dontEnums[i]);
+                  }
+                }
+              }
+              return result;
+            };
+          }());
+        }
+
+        return Object.keys(object);
+      })(this.ports);
+
+      return ((!secure) ? protocols[0]: protocols[1])
+        + '://' + this.domain + ':'
+        + ((!secure) ? this.ports['ws']: this.ports['wss']);
+    },
+
+    clockRate: 1000,
+
+    v: function() {
+      return '0.0.1';
+    },
+
+    init: function() {
+      this.wait();
+    },
+
+    wait: function() {
+      if (typeof _beng$wid !== 'undefined') {
+        this._beng.$wid = _beng$wid;
+      }
+
+      if (this._beng.$wid !== null) {
+        var ws = _protected.create(this.getPath());
+
+        if (ws != false) {
+          _protected.connect(ws, this.clockRate, this._beng);
+        }
+      } else {
+        setTimeout(this.wait.bind(this), 250);
+      }
+    },
+  };
+
+  return Object.freeze(bengala);
+})();
+
+_bengala.init();
